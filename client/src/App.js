77d75d798462b8ebe55b0783353829f6142f110a
IlliaSmolin <55273908+IlliaSmolin@users.noreply.github.com>
2020-07-21 19:12:33 +0300

    added and configured navbar via react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import { NavLink, Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-  state = { users: [] };
+const App = () => (
+  <div className="app">
+    <h1>Navigation Bar by React Router</h1>
+    <Navigation />
+    <Main />
+  </div>
+);
 
-  componentDidMount() {
-    fetch("/users")
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/users'>Users</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/something'>Something</NavLink></li>
+    </ul>
+  </nav>
+);
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        <ul>
-          {this.state.users.map(user => 
-            <li key = {user.id}>{user.username}</li>)}
-        </ul>
-      </div>
-    )
-  }
-}
+const Main = () => (
+  <Switch>
+    <Route exact path='/' component={Home}></Route>
+    <Route exact path='/users' component={Users}></Route>
+    <Route exact path='/something' component={Something}></Route>
+  </Switch>
+);
+
+const Home = () => (
+  <div className='home'>
+    <h1>Welcome to "Problems Practice" website</h1>
+    <p>Here I will implement back-front communication to interactively solve interesting problems</p>
+  </div>
+);
+
+const Users = () => (
+  <div className='users'>
+    <h1>Users</h1>
+    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+  </div>
+);
+
+const Something = () => (
+  <div className='something'>
+    <h1>Contact Me</h1>
+    <p>You can reach me via email: <strong>hello@example.com</strong></p>
+  </div>
+);
 
 export default App;
