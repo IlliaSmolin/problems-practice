7f5c24e791f5d3c9fad5c0381a5c59a824d3f21a
IlliaSmolin <55273908+IlliaSmolin@users.noreply.github.com>
2020-09-28 21:23:07 +0300

    added state properties to control input values

--- client/src/components/BruteForce.js
+++ client/src/components/BruteForce.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
 
 class BruteForce extends Component {
-  state = { users: [] };
+  constructor() {
+    super();
+    this.state = {
+      value: "",
+      response: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
   sendPOST = () => {
     const request = {
       method: 'POST',
       headers: {'Content-Type': 'Application/json'},
-      body: JSON.stringify({a: "test"})
+      body: JSON.stringify({a: this.state.value})
     };
     
     fetch("/bruteforce", request)
       .then((res) => res.json())
-      .then((users) => this.setState({ users }));
+      .then((response) => this.setState({ response }));
   }
   
   render() {
@@ -22,10 +34,10 @@ class BruteForce extends Component {
           This page's gonna help you to realize how quick your password can be cracked
         </p>
         <h1>Here's the input</h1>
-        <input className="toSend" type="text"></input>
+        <input id="toSend" type="text" value={this.state.value} onChange={this.handleChange}></input>
         <button onClick={this.sendPOST}>Calculate</button>
         <h2>Result:</h2>
-        <input className="toReceive" disabled type="text"></input>
+        <input id="toReceive" disabled type="text" value={this.state.response}></input>
       </div>
     )
   }
